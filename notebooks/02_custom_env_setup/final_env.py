67db370e230c26fbe8a4ff2042d553e65a2cfe43
Simren Basra <simrenbasra@gmail.com>
2024-12-07 11:12:20 +0000

    Update to reward structure

--- notebooks/02_custom_env_setup/final_env.py
+++ notebooks/02_custom_env_setup/final_env.py
@@ -285,8 +285,8 @@ class Final_Haunted_Mansion(gym.Env):
         else:
             # Check if the agent encounters a ghost
             if any(np.array_equal(self.agent_location, ghost) for ghost in self.ghosts_location):
-                # Penalty of - 7 (-7 is the max penalty, to increase beyond this causes agent to get stuck!)
-                reward -= 7
+                # Penalty of - 15
+                reward -= 15
             
             for candy in self.candies_location:
                 if np.array_equal(self.agent_location, candy) and not np.array_equal(candy, [-1, -1]):
